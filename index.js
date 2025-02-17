66bee3a418d4a5d059b62e2401016b10e5be9177
shirish-raj-gupta <codersrg@gmail.com>
2025-02-18 00:36:53 +0530

    made task manager at basic

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ app.get('/files/:filename', function(req,res){
   })
 })
 
+app.get('/edit/:filename', function(req,res){
+  res.render('edit' , {filename : req.params.filename});
+})
+
+app.post('/edit', function(req,res) {
+  fs.rename(`./files/${req.body.previous}` , `./files/${req.body.new}`, function(err){
+    res.redirect("/");
+    console.log(req.body);
+  })
+})
+
 app.post('/create', function(req,res) {
   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details, function(err){
     res.redirect("/")
